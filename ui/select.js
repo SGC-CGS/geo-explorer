00967ea09e017fc5317772513b7ed17688e8d773
maddixm <m.maddix@gmail.com>
2021-04-19 11:08:34 -0400

    Document ui select.

--- ui/select.js
+++ ui/select.js
@@ -5,8 +5,16 @@ import Dom from '../tools/dom.js';
 
 import Templated from '../components/templated.js';
 
+/**
+ * Select box module
+ * @module ui/select
+ * @extends Templated
+ */
 export default Core.Templatable("Basic.Components.Select", class Select extends Templated {
 	
+	/**
+	 * Get/set select box value
+	 */
 	get value() {
 		return this.Elem("root").value;
 	}
@@ -15,6 +23,9 @@ export default Core.Templatable("Basic.Components.Select", class Select extends 
 		this.Elem("root").value = value;
 	}
 	
+	/**
+	 * Get/set disabled value
+	 */
 	set disabled(value) {
 		this.Elem("root").disabled = value;
 	}
@@ -23,12 +34,18 @@ export default Core.Templatable("Basic.Components.Select", class Select extends 
 		return this.Elem("root").disabled;
 	}
 	
+	/**
+	 * Get selected element object (label, value)
+	 */
 	get selected() {
 		var i = this.Elem("root").value;
 		
 		return this._items[i];
 	}
 	
+	/**
+	 * Set initial value of select box
+	 */
 	set placeholder(value) {
 		this._ph = Dom.Create("option", { innerHTML:value, value:-1, className:"select-placeholder" });
 		
@@ -38,6 +55,12 @@ export default Core.Templatable("Basic.Components.Select", class Select extends 
 		this.Elem("root").insertBefore(this._ph, this.Elem("root").firstChild);
 	}
 	
+	/**
+	 * Call constructor of base class (Templated) and initialize select
+	 * @param {object} container - div container and properties
+	 * @param {object} options - any additional options to assign to the widget (not typically used)
+	 * @returns {void}
+	 */	
 	constructor(container, options) {
 		super(container, options);
 		
@@ -48,16 +71,33 @@ export default Core.Templatable("Basic.Components.Select", class Select extends 
 		this.Node("root").On("change", this.OnSelect_Change.bind(this));
 	}
 	
+	/**
+	 * Populate options in select box
+	 * @param {string} label - Label of select option
+	 * @param {string} title - Title of select option
+	 * @param {object} item - Object containing option description, label, and value
+	 * @returns {void}
+	 */
 	Add(label, title, item) {
 		Dom.Create("option", { innerHTML:label, value:this._items.length, title:title }, this.Elem("root"));
 		
 		this._items.push(item);
 	}
 	
+	/**
+	 * Call from delegate to FindIndex
+	 * @param {function} delegate  - Delegate function
+	 * @returns {void}
+	 */
 	Select(delegate) {		
 		this.value = this.FindIndex(delegate);
 	}
 	
+	/**
+	 * Finds index of selected classification method through link to styler widget
+	 * @param {function} delegate - Delegate function
+	 * @returns {number} Index number of selected classification method (-1 if none)
+	 */
 	FindIndex(delegate) {
 		for (var i = 0; i < this._items.length; i++) {
 			if (delegate(this._items[i], i)) return i;
@@ -66,16 +106,29 @@ export default Core.Templatable("Basic.Components.Select", class Select extends 
 		return -1;
 	}
 	
+	/**
+	 * Emit change event when a new option is selected
+	 * @param {object} ev - Event object
+	 * @returns {void}
+	 */
 	OnSelect_Change(ev) {
 		var item = this._items[ev.target.value];
 		
 		this.Emit("Change", { index:ev.target.value, item:item, label:ev.target.innerHTML });
 	}
 	
+	/**
+	 * Create HTML for select element
+	 * @returns {string} HTML for select element
+	 */
 	Template() {
 		return '<select handle="root"></select>';
 	}
 	
+	/**
+	 * Empty items in select box
+	 * @returns {void}
+	 */
 	Empty() {
 		Dom.Empty(this.Elem("root"));
 		
