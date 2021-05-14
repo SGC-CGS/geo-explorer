b9e6aef0f903aad6f777d7e96a8f5dc1e491516e
elaf.selim <Elaf.selim@cloud.statcan.ca>
2021-05-14 15:36:19 -0400

    GEOMS-2936 - CSGE - Add an invisible instructions text

--- widgets/selector.js
+++ widgets/selector.js
@@ -46,7 +46,9 @@ export default Core.Templatable("App.Widgets.Selector", class Selector extends T
 		nls.Add("Dropdowns_Title", "en", "Select indicator to display on the map:");
 		nls.Add("Dropdowns_Title", "fr", "Sélectionnez l'indicateur à afficher sur la carte:");		
 		nls.Add("UpdateMapBtn_Title", "en", "Update Map");
-		nls.Add("UpdateMapBtn_Title", "fr", "Recharger la Carte");		
+        nls.Add("UpdateMapBtn_Title", "fr", "Recharger la Carte");	
+        nls.Add("AppDescription", "en", "This map will display a thematic map according to the indicator selected. Use the dropdown lists below to select dimension members for each dimensions of the current CODR product. Then, click on the 'update map' button to display the thematic map. A data table at the bottom contains the data shown on the map.");
+        nls.Add("AppDescription", "fr", "Cette carte affichera une carte thématique en fonction de l'indicateur sélectionné. Utilisez les listes déroulantes ci-dessous pour sélectionner des membres de dimension pour chaque dimension du produit CODR actuel. Ensuite, cliquez sur le bouton 'mettre à jour la carte' pour afficher la carte thématique. Un tableau de données en bas contient les données affichées sur la carte.");
 	}
 	
     constructor(container, options) {
@@ -151,6 +153,7 @@ export default Core.Templatable("App.Widgets.Selector", class Selector extends T
         
     Template() {
         return 	"<h2 handle='dropdowntitle' class='col-md-12 mrgn-tp-sm hidden'>nls(Dropdowns_Title)</h2>" +
+                "<div class='text-center'><a href='#' class='wb-inv wb-show-onfocus wb-sl'>nls(AppDescription)</a></div>" +            
 				"<div handle='dimensions' class='dimensions'></div>" +
 				"<div class='row mrgn-tp-sm'>" +
 					"<button handle='submit' type='submit' class='btn btn-primary col-md-2 pull-right hidden'>nls(UpdateMapBtn_Title)</button>" +
