fad019b7a4810ad8ebde9451d280be9052385fae
omar kawach <omarkawach@outlook.com>
2021-06-30 20:26:24 -0400

    pubsub created

---
+++ tools/pubsub.js
@@ -0,0 +1,38 @@
+let events = [];
+
+/**
+ * PubSub module
+ * @module tools/pubsub
+ * @description to facilitate communication between modules
+ */
+export default class PubSub {
+    /**
+     * Add an event that has a name and function
+     * @param {*} eventName - Name of the event
+     * @param {*} fn - The function for the event
+     */
+    static Add(eventName, fn) {
+        events.push({eventName, fn});
+    }
+
+    /**
+     * Remove an event based on the event name
+     * @param {*} eventName - Name of the event
+     */
+    static Remove(eventName) {
+        events = events.filter(event => event.eventName != eventName);
+    }
+
+    /**
+     * Run the function for the event
+     * @param {*} eventName - Name of the event
+     * @param {*} arg - A single argument for the function 
+     */
+    static Emit(eventName, arg) {
+        for(let event of events) {
+            if (event.eventName == eventName) {
+                event.fn(arg);
+            }
+        }
+    }
+}
